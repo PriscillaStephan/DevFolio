d7ee2639ec07720d8cd21c9e01337532d0ee397f
unknown <pressistephan@gmail.com>
2021-04-19 22:34:51 +0300

    greeting lottie updated

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -3,6 +3,7 @@ import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.css";
 import landingPerson from "../../assets/lottie/landingPerson";
+import workAtNightDark from "../../assets/lottie/workAtNightDark";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
@@ -50,7 +51,7 @@ export default function Greeting() {
           </div>
           <div className="greeting-image-div">
             {illustration.animated ? (
-              <DisplayLottie animationData={landingPerson} />
+              <DisplayLottie animationData={workAtNightDark} />
             ) : (
               <img
                 alt="man sitting on table"
